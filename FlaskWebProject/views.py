2658e96d6e11571285053d01ee8769fc09617a93
anthanh <thanhan.lan@gmail.com>
2024-06-12 09:52:00 +0700

    update authorized

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -89,7 +89,7 @@ def authorized():
         # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args.get('code'),
-            scope=Config.SCOPE,
+            scopes=Config.SCOPE,
             redirect_uri=url_for('authorized', _external=True, _scheme='https')
         )
         if "error" in result:
